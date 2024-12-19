cb0659582b06beb64b6d79e997fbdcf9e2e23ca0
Akshay E <akshaykumar2002817@gmail.com>
2024-12-19 10:11:01 +0530

    dec-19 third commit

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://akshaye.online/',
-  frontendUrl: 'https://findy-job.vercel.app/',
+  backendUrl: 'https://akshaye.online',
+  frontendUrl: 'https://findy-job.vercel.app',
   ZEGO_APP_ID: 798461473, 
   ZEGO_SERVER_SECRET: '5f44ce6f6b0921ab0d8fc24ea57c3936'
 };
