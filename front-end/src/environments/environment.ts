211257e0d67b6263e73a5f90617021b87f4567f2
Akshay E <akshaykumar2002817@gmail.com>
2025-01-15 18:36:25 +0530

    fixing zego cloud - 1

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: false,
-  // backendUrl: 'https://akshaye.online',
-  // frontendUrl: 'https://findy-job.vercel.app',
-  backendUrl: 'http://localhost:8888',
-  frontendUrl: 'http://localhost:4200',
+  backendUrl: 'https://akshaye.online',
+  frontendUrl: 'https://findy-job.vercel.app',
   ZEGO_APP_ID: 1634351379, 
   ZEGO_SERVER_SECRET:'d3a4ab3241a29e86eefead0fd6b69ae7'
 };
